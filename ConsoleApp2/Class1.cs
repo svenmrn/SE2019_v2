31b0c7568f7f05d1cc9846d9ebcb129b04f48f98
svenmrn <32125602+svenmrn@users.noreply.github.com>
2019-09-23 17:01:15 +0200

    aangepast naar 10 * 2

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2
         int CalcNumberOfDaysTillEndOfSchoolYear()
         {
             //TODO: calculate ..
-            return 888;
+            return 10 * 2;
         }
     }
 }
