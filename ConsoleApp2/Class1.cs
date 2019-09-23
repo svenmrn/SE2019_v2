810127fa389b53cdd78330c0df76824ffa6a2b86
Sven <sven.marien01@ap.be>
2019-09-23 16:59:38 +0200

    Auto stash before merge of "master" and "origin/master"

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp2
         int CalcNumberOfDaysTillEndOfSchoolYear()
         {
             //TODO: calculate ..
-            return 555;
+            return 888;
         }
     }
 }
