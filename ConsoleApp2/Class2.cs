9c91f23f7ff0f3c4aeb406319912268f3c01d34c
svenmrn <32125602+svenmrn@users.noreply.github.com>
2019-09-27 10:01:18 +0200

    Update Class2.cs
    foutje opgelost

--- ConsoleApp2/Class2.cs
+++ ConsoleApp2/Class2.cs
@@ -6,9 +6,10 @@ namespace ConsoleApp2
 {
     class Class2
     {
+         void Add2Numbers(int a, int b){
+        return a+b;
+         }
     }
     
-    void Add2Numbers(int a, int b){
-        return a+b;
-    }
+   
 }
